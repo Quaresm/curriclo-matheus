9cab7680a76240164756a78b12e19c897198c181
Quaresm <matheusssquaresma@gmail.com>
2022-04-26 09:49:26 -0300

    Update Template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import MainPage from './pages/mainpage';
+import Header from '.header/Header'
+import Navbar from './navbar/Navbar'
+import Footer from './footer/Footer'
+import About from './components/about/About';
+import Education from './components/education/Education';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Contact from './components/contact/Contact';
 
 
 const App = () => {
-  return <MainPage />
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <About />
+      <Experience />
+      <Education />
+      <Services />
+      <Contact />
+      <Footer />
+    </>
+    )
 }
 
 export default App
