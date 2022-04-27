c02e76ed532bff167cd37f7e720b3043ba20b8aa
Quaresm <matheusssquaresma@gmail.com>
2022-04-27 09:01:33 -0300

    Css concluded

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/foto.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="container header__container">
+      <h5> Olá eu sou o </h5>
+      <h1> Matheus Quaresma </h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      <CTA />
+
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+    </div>
   )
 }
 
